e2755904f990bcf7b66748598ca0852d4ef1a1a0
avezqureshi <aveznqureshi14@gmail.com>
2024-05-15 13:06:37 +0530

    first commit

--- server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var auth_1 = require("../controllers/auth");
-var router = express.Router();
-router.post('/authenticate', auth_1.authenticateUser);
-exports.default = router;
